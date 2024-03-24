f7414f29018835e9ab8831b24bd7ea6b690650d3
Peter <pivow@hotmail.com>
2024-03-24 19:56:40 -0300

    Profile finished

--- spa/src/config.js
+++ spa/src/config.js
@@ -1,5 +1,5 @@
 // config.js
-const apiBaseUrl = "http://192.168.1.9/templates/laravel-rest-api-and-react-app/Api/public/api";
-const storageUrl = "http://192.168.1.9/templates/laravel-rest-api-and-react-app/Api/public/storage";
+const apiBaseUrl = "http://192.168.1.9/cutinapp1.0/Api/public/api";
+const storageUrl = "http://192.168.1.9/cutinapp1.0/Api/storage/app/public/";
 
 export { apiBaseUrl, storageUrl };
