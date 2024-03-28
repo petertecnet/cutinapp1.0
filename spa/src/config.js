9a66e8822c5c5dd12e9f5c2c55482dda614e5e33
Peter <pivow@hotmail.com>
2024-03-28 01:38:18 -0300

    nothing

---
+++ spa/src/config.js
@@ -0,0 +1,8 @@
+// config.js
+
+const apiBaseUrl = "http://192.168.1.6/cutinapp1.0/Api/public/api";
+const storageUrl = "http://192.168.1.6/cutinapp1.0/Api/storage/app/public/";
+
+
+
+export { apiBaseUrl, storageUrl };
