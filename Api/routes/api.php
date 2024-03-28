13e59549fe6ff717f5f628ed9e34d5224828028a
Peter <pivow@hotmail.com>
2024-03-27 23:32:59 -0300

    Mergin conflicts

--- Api/routes/api.php
+++ Api/routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\{AuthController, UserController, ProfileController};
+use App\Http\Controllers\{AuthController, UserController, ProfileController, ProductionController};
 
 Route::group([
     'middleware' => 'api',
@@ -26,20 +26,35 @@ Route::group([
     'prefix' => 'user'
 ], function ($router) {
     Route::get('/', [UserController::class, 'list'])->name('user.list');
-    Route::get('/{id}', [UserController::class, 'show'])->name('user.show'); 
+    Route::get('/show/{id}', [UserController::class, 'show'])->name('user.show'); // Rota para mostrar o perfil do usuário
+    Route::get('/{userName}', [UserController::class, 'view'])->name('user.view'); // Rota para visualizar as informações do usuário por userName
     Route::post('/new', [UserController::class, 'store'])->name('user.store');
     Route::post('/{user}', [UserController::class, 'update'])->name('user.update'); // Deve ser post por causa da imagem
     Route::delete('/{id}', [UserController::class, 'destroy'])->name('user.destroy');
 });
 
+
+
 Route::group([
     'middleware' => 'api',
     'prefix' => 'profile'
 ], function ($router) {
-
     Route::get('/', [ProfileController::class, 'list'])->name('profile.list');
     Route::get('/{id}', [ProfileController::class, 'show'])->name('profile.show');
     Route::post('/', [ProfileController::class, 'store'])->name('profile.store');
-Route::put('/{id}', [ProfileController::class, 'update'])->name('profile.update');
+    Route::put('/{id}', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/{id}', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'production'
+], function ($router) {
+    Route::post('/', [ProductionController::class, 'store'])->name('production.store');
+    Route::get('/', [ProductionController::class, 'list'])->name('production.list');
+    Route::get('/{id}', [ProductionController::class, 'show'])->name('production.show'); // Rota para mostrar os detalhes de uma produção específica
+    Route::post('/{id}', [ProductionController::class, 'update'])->name('production.update');
+    Route::delete('/{id}', [ProductionController::class, 'delete'])->name('production.delete'); // Rota para excluir uma produção
+    Route::get('/view/{slug}', [ProductionController::class, 'view'])->name('production.view'); // Rota para visualizar uma produção por slug
+    // Adicione outras rotas de produção conforme necessário
 });
